a9cfad72b1686f1baa9e851b3e37c6b0002d42fd
siiii <siiiiiiiiiiya@gmail.com>
2021-12-23 21:32:39 +0500

    Выполнено задание. Ищет количество слов с последней гласной буквой

---
+++ vowels/vowels/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+namespace vowels
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите полное имя файла");
+            var fileName = Console.ReadLine();
+            string[] text;
+
+            Console.WriteLine();
+
+            if (File.Exists(fileName))
+                text = File.ReadAllLines(fileName);
+            else
+            {
+                Console.WriteLine("Такого файла не существует");
+                Console.ReadKey();
+                return;
+            }
+
+
+            Console.WriteLine(CountWordsByVowel(text));
+
+            Console.ReadKey();
+
+        }
+
+        static int CountWordsByVowel(string[] lines)
+        {
+            string[] vowels = { "а", "е", "ё", "и", "о", "у", "э", "ю", "я" };
+            return lines
+                .SelectMany(line => line.Split(' ', '.', ',', ';', ':', '!', '?', '*', '—', '"', '«', '»'))
+                .Where(w => w.Length > 0 && char.IsLetter(w[0]))
+                .Select(w => w.ToLower())
+                .Where(w => vowels.Any(v => w.EndsWith(v, StringComparison.OrdinalIgnoreCase)))
+                .ToList()
+                .Count();
+        }
+    }
+}
